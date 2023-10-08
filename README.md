14ba2e1a6c5d8e2903e6dfda7071c725b33186c4
Edizon <edizon1987@gmail.com>
2023-10-08 16:04:40 -0500

    update - avance curso clase 125

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # control-gastos
-control de gastos react
+#Nombre del proyecto "Control de gastos react"
+
+Avances de la semana 02-06 Octubre 2023
+#Clase 125.- Personalizar La Gráfica circular con estilos Semana 
+- En esta clase se añadieron 2 importaciones  CircularProgressbar, buildStyles que permiten añadir estilos a la grafica circular, se modifico el color azul del circulo y trail color que modifica el fondo de color gris en el centro.
+
+Las importaciones se realizaron en las lineas 2 y 3 y se utilizarón en el componente CircularProgressbar de las lineas 35 y 39.
--- src/components/ControlPresupuesto.tsx
+++ src/components/ControlPresupuesto.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import "react-circular-progressbar/dist/styles.css"
+
 const ControlPresupuesto = ({gastos,presupuesto}) => {
    
    const [porcentaje,setPorcentaje] = useState(0)
@@ -32,6 +33,10 @@ const ControlPresupuesto = ({gastos,presupuesto}) => {
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
          <div>
             <CircularProgressbar 
+             styles={buildStyles({
+               pathColor:'#3B82F6',
+               trailColor:'#F5F5F5'
+             })}
              value={porcentaje}
             />
          </div>
